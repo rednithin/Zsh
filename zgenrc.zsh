a28ffa4c4b823a383bc003e030016147a3c71972
rednithin <reddy.nithinpg@live.com>
2019-01-30 20:01:55 +0530

    Switched to fast syntax highlighting.

--- zgenrc.zsh
+++ zgenrc.zsh
@@ -16,9 +16,11 @@ if ! zgen saved; then
     zgen load zsh-users/zsh-completions
     zgen load zsh-users/zsh-autosuggestions
 
-    zgen load denysdovhan/spaceship-prompt
-    # zgen load rednithin/honukai-iterm-zsh
-    zgen load zsh-users/zsh-syntax-highlighting
+    # zgen load denysdovhan/spaceship-prompt
+    zgen load rednithin/honukai-iterm-zsh
+    
+    # zgen load zsh-users/zsh-syntax-highlighting
+    zgen load zdharma/fast-syntax-highlighting
     zgen load zsh-users/zsh-history-substring-search
     
     zgen save
