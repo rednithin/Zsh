d2aa0a20ce0145668763010fc34575cd5f259696
rednithin <reddy.nithinpg@live.com>
2019-09-08 12:30:13 +0530

    Updated path.

--- path.zsh
+++ path.zsh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 me=$(whoami)
 
-my_paths=("/home/$me/.yarn/bin" "/opt/miniconda3/bin/" "usr/local/miniconda3/bin/")
+my_paths=("/home/$me/.yarn/bin" "/opt/miniconda3/bin/" "usr/local/miniconda3/bin/" "/home/$me/miniconda3/bin")
 
 for my_path in $my_paths; do
   if [ -d $my_path ]; then
